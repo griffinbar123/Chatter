c9ffa2e1a0ce21650a4c79103bbcbd22b89d9482
griffinbar123 <gjb20b@acu.edu>
2023-04-29 01:02:08 -0500

    Initial Commit

---
+++ Chatter/ChatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterApp.swift
+//  Chatter
+//
+//  Created by Griffin Barnard on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
