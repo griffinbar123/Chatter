2ff7be4fabc269d0700fba45ecff1c97c802f312
griffinbar123 <gjb20b@acu.edu>
2023-04-30 00:25:35 -0500

    added animation and stuff

--- Chatter/EntryScreen.swift
+++ Chatter/EntryScreen.swift
@@ -8,27 +8,107 @@
 import SwiftUI
 
 struct EntryScreen: View {
+    @State var showButton: Bool = false;
     var body: some View {
         ZStack{
             VStack {
                 LogoAsMain
-                TagLine
-                Spacer()
-                VStack {
-                    loginButton
-                        .padding(.bottom, 16)
-                    signupButton
-                        .padding(.bottom, 32)
-                    Text("We store a freakin massive amount of your data. I mean, how else do you expect us to provide you with the most elegant matches?")
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .frame(width: UIScreen.main.bounds.width*0.7)
-                        .font(.system(size:12))
-                        .padding(.bottom, 40)
+                
+                if(showButton){
+                    TagLine
+                    Spacer()
+                    logorsign
                 }
+                
+            
             }
         }.frame(maxWidth: .infinity, maxHeight: .infinity) .background(BackgroundGradient)
-
+            .animation(Animation.easeOut(duration: 0.7), value: showButton)
+            .onAppear{
+                withAnimation {
+                    showButton = true
+                }
+        }
+    }
+    
+    var logorsign: some View {
+        VStack {
+            appleSignIn
+                .padding(.bottom, 8)
+            facebookSignIn
+                .padding(.bottom, 8)
+            phoneSignIn
+                .padding(.bottom, 16)
+            Text("We store a freakin massive amount of your data. I mean, how else do you expect us to provide you with the most elegant matches?")
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .frame(width: UIScreen.main.bounds.width*0.7)
+                .font(.system(size:12))
+                .padding(.bottom, 40)
+        }
+    }
+    
+    var appleSignIn: some View {
+        Button(action: {
+            print(UIScreen.main.bounds.width)
+        }, label: {
+            HStack{
+                Image(systemName: "apple.logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width:15, height:15)
+                    .offset(y:-1)
+                Text("Continue with Apple")
+                    
+            }.padding()
+                .frame(width: UIScreen.main.bounds.width*0.8)
+                .background(
+                    Color.black
+                        .cornerRadius(90))
+                .foregroundColor(.white)
+                .font(.system(size:16))
+                .bold()
+                
+        })
+    }
+    
+    var facebookSignIn: some View {
+        Button(action: {
+            print(UIScreen.main.bounds.width)
+        }, label: {
+            HStack{
+                Image(systemName: "apple.logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width:15, height:15)
+                    .offset(y:-1)
+                Text("Continue with Facebook")
+                    
+            }.padding()
+                .frame(width: UIScreen.main.bounds.width*0.8)
+                .background(
+                    Color("FacebookBlue")
+                        .cornerRadius(90))
+                .foregroundColor(.white)
+                .font(.system(size:16))
+                .bold()
+                
+        })
+    }
+    
+    var phoneSignIn: some View {
+        Button(action: {
+            print(UIScreen.main.bounds.width)
+        }, label: {
+            Text("Continue with Phone Number")
+                .padding()
+                .frame(width: UIScreen.main.bounds.width*0.8)
+                .background(
+                    Color.white
+                    .cornerRadius(90))
+                .font(.system(size:16))
+                
+        })
     }
     
     var loginButton: some View {
@@ -46,22 +126,22 @@ struct EntryScreen: View {
         })
     }
     
-    var signupButton: some View {
-        Button(action: {
-            print(UIScreen.main.bounds.width)
-        }, label: {
-            Text("Sign Up")
-                .padding()
-                .frame(width: UIScreen.main.bounds.width*0.8)
-                .foregroundColor(.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 90)
-                        .stroke(Color.white, lineWidth: 2)
-                )
-                .font(.system(size:16))
-                
-        })
-    }
+//    var signupButton: some View {
+//        Button(action: {
+//            print(UIScreen.main.bounds.width)
+//        }, label: {
+//            Text("Sign Up")
+//                .padding()
+//                .frame(width: UIScreen.main.bounds.width*0.8)
+//                .foregroundColor(.white)
+//                .overlay(
+//                    RoundedRectangle(cornerRadius: 90)
+//                        .stroke(Color.white, lineWidth: 2)
+//                )
+//                .font(.system(size:16))
+//
+//        })
+//    }
 }
 
 struct EntryScreen_Previews: PreviewProvider {
